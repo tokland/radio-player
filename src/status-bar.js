94f1bf0e7cbf9e740c5c71179c02a0b4b0479726
Arnau Sanchez <pyarnau@gmail.com>
2017-12-05 00:58:24 +0100

    relative status bar

--- src/status-bar.js
+++ src/status-bar.js
@@ -5,7 +5,7 @@ import {SwipeView} from 'react-native-swipe-view';
 import _ from 'lodash';
 
 const styles = {
-  wrapper: {position: "absolute", bottom: 0, left: 0, width: "100%"},
+  wrapper: {position: "relative", bottom: 0, left: 0, width: "100%"},
   main: {flexDirection: "row", height: 70, backgroundColor: '#222'},
   logo: {width: 50, height: 60, margin: 5, justifyContent: 'center'},
   name: {flex: 1, padding: 5, fontSize: 16, lineHeight: 40, textAlign: 'center',
@@ -19,7 +19,7 @@ const images = {
 };
 
 renderStatusBar = ({station, status, isPlaying, onPress, onSwipedOut}) => (
-  <SwipeView onSwipedOut={onSwipedOut} style={styles.wrapper}>
+  <SwipeView onSwipedOut={onSwipedOut} style={styles.wrapper} minPanToComplete={0.25}>
     <View style={styles.main}>
       <Image source={station.logo_source} style={styles.logo} />
       <Text style={styles.name}>
